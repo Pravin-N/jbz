ae4b0fad7a72dc937b61dac3c4183ba9aafbb6c2
Pravin N <pravin86n@gmail.com>
2021-04-12 18:25:23 -0400

    testing with chat tawk

--- pages/_document.js
+++ pages/_document.js
@@ -68,7 +68,7 @@ class MyDocument extends Document {
             integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
             crossOrigin="anonymous"
           ></script>
-          {/* <script type="text/javascript" src="/js/chat.js"></script> */}
+          <script type="text/javascript" src="/js/chat.js"></script>
         </body>
       </Html>
     );
