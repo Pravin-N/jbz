c4879e5924748d7238ea884a13fec0d853a3f3ee
pravin86n <pravin86n@gmail.com>
2021-04-18 14:42:06 -0400

    sitemap

--- next.config.js
+++ next.config.js
@@ -4,5 +4,10 @@ module.exports = {
   images: {
     domains: ["images.ctfassets.net"],
   },
-
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      require("./scripts/generate-sitemap");
+    }
+    return config;
+  },
 };
---
+++ scripts/generate-sitemap.js
@@ -0,0 +1,75 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+import FeaturePostData from "../components/data";
+import articles from "../components/articles";
+
+const featuredata = FeaturePostData.map(
+  (feature) => "/business-setup/" + feature.slug
+);
+const articleData = articles.map(
+  (article) => "/dubai-business-setup-blogs/" + article.slug
+);
+
+const fs = require("fs");
+
+const globby = require("globby");
+const prettier = require("prettier");
+
+(async () => {
+  const prettierConfig = await prettier.resolveConfig("./.prettierrc.js");
+
+  // Ignore Next.js specific files (e.g., _app.js) and API routes.
+  const pages = await globby([
+    "pages/**/*{.js,.mdx}",
+    "!pages/_*.js",
+    "!pages/api",
+    "pages/**/[slug]",
+  ]);
+  const sitemap = `
+        <?xml version="1.0" encoding="UTF-8"?>
+        <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+            ${pages
+              .map((page) => {
+                const path = page
+                  .replace("pages", "")
+                  .replace(".js", "")
+                  .replace(".mdx", "");
+                const route = path === "/index" ? "" : path;
+
+                return `
+
+                        <url>
+                            <loc>${`https://jbz.vercel.app${route}`}</loc>
+                        </url>
+                    `;
+              })
+              .join("")}
+
+              ${featuredata
+                .map((page) => {
+                  return `
+                          <url>
+                              <loc>${`https://jbz.vercel.app${page}`}</loc>
+                          </url>
+                      `;
+                })
+                .join("")}
+                ${articleData
+                  .map((page) => {
+                    return `
+                              <url>
+                                  <loc>${`https://jbz.vercel.app${page}`}</loc>
+                              </url>
+                          `;
+                  })
+                  .join("")}
+        </urlset>
+    `;
+
+  // If you're not using Prettier, you can remove this.
+  const formatted = prettier.format(sitemap, {
+    ...prettierConfig,
+    parser: "html",
+  });
+
+  fs.writeFileSync("public/sitemap.xml", formatted);
+})();
