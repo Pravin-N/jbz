634cd0d32b2f652fa42280672bf4b329c4f13a21
pravin86n <pravin86n@gmail.com>
2021-04-18 17:46:54 -0400

    sitemap

--- next.config.js
+++ next.config.js
@@ -4,4 +4,11 @@ module.exports = {
   images: {
     domains: ["images.ctfassets.net"],
   },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      require("./generate-sitemap");
+    }
+
+    return config;
+  },
 };
