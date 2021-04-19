dd6d6fe6cc9dd1b1d1c0b41bdea09ce52c2dba45
pravin86n <pravin86n@gmail.com>
2021-04-19 13:08:55 -0400

    font awesome fix

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import "../styles/global.css";
 import "../styles/videomodal.css";
 import "../styles/style.css";
 import "../styles/custom.css";
+import "font-awesome.min.css";
 
 import Layout from "../components/Layout/Layout";
 
