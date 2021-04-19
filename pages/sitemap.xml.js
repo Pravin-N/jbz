5f2c4b9da5779c72cd37cc0d6117749759b40ca9
pravin86n <pravin86n@gmail.com>
2021-04-19 11:02:08 -0400

    sitemap

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -2,7 +2,7 @@ import React from "react";
 import globby from "globby";
 import fs from "fs";
 
-const EXTERNAL_DATA_URL = "https://jbz.vercel.app/";
+const EXTERNAL_DATA_URL = "https://jbz.vercel.app/posts";
 
 const createSitemap = (posts, pages) => `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
