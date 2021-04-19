7cc7f43ab9f487b7f2ff53d9aef7030fddf47a50
pravin86n <pravin86n@gmail.com>
2021-04-19 11:09:32 -0400

    sitemap

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -1,9 +1,8 @@
 import React from "react";
-import globby from "globby";
 
 const EXTERNAL_DATA_URL = "https://jbz.vercel.app/posts";
 
-const createSitemap = (posts, pages) => `<?xml version="1.0" encoding="UTF-8"?>
+const createSitemap = (posts) => `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
         ${posts
           .map(({ id }) => {
@@ -14,21 +13,7 @@ const createSitemap = (posts, pages) => `<?xml version="1.0" encoding="UTF-8"?>
                 `;
           })
           .join("")}
-          ${pages
-            .map((page) => {
-              const path = page
-                .replace("pages", "")
-                .replace(".js", "")
-                .replace(".mdx", "");
-              const route = path === "/index" ? "" : path;
-
-              return `
-                      <url>
-                          <loc>${`https://jbz.vercel.app${route}`}</loc>
-                      </url>
-                  `;
-            })
-            .join("")}
+          
     </urlset>
     `;
 
@@ -36,15 +21,9 @@ class Sitemap extends React.Component {
   static async getInitialProps({ res }) {
     const request = await fetch(EXTERNAL_DATA_URL);
     const posts = await request.json();
-    const pages = await globby([
-      "./pages/**/*{.js,.mdx}",
-      "!./pages/_*.js",
-      "!./pages/api",
-      "!./pages/**/[slug]*{.js,.mdx}",
-    ]);
 
     res.setHeader("Content-Type", "text/xml");
-    res.write(createSitemap(posts, pages));
+    res.write(createSitemap(posts));
     res.end();
   }
 }
