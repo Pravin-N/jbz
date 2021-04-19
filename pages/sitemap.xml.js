0c2ef55069d74e961bf3184549984bffde54380f
pravin86n <pravin86n@gmail.com>
2021-04-19 11:16:33 -0400

    sitemap

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -1,9 +1,32 @@
 import React from "react";
+import fs from "fs";
+import globby from "globby";
 
 const EXTERNAL_DATA_URL = "https://jbz.vercel.app/posts";
 
+const pages = await globby([
+  "pages/**/*{.js,.mdx}",
+  "!pages/_*.js",
+  "!pages/api",
+]);
+
 const createSitemap = (posts) => `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    ${pages
+      .map((page) => {
+        const path = page
+          .replace("pages", "")
+          .replace(".js", "")
+          .replace(".mdx", "");
+        const route = path === "/index" ? "" : path;
+
+        return `
+                <url>
+                    <loc>${`https://yoursitehere.com${route}`}</loc>
+                </url>
+            `;
+      })
+      .join("")}
         ${posts
           .map(({ id }) => {
             return `
