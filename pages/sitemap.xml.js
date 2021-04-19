694a2693b2098cf0ee95072604249885bc6d6614
pravin86n <pravin86n@gmail.com>
2021-04-19 12:28:11 -0400

    sitemap

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -1,8 +1,17 @@
 import React from "react";
+import globby from "globby";
+import FeaturePostData from "../components/data";
+import articles from "../components/articles";
 
-const EXTERNAL_DATA_URL = "https://jbz.vercel.app/posts";
+const MAIN_URL = "https://jbz.ae";
+const FEATURE_URL = "https://jbz.ae/business-setup";
+const BLOG_URL = "https://jbz.ae/dubai-business-setup-blogs";
 
-const createSitemap = (posts) => `<?xml version="1.0" encoding="UTF-8"?>
+const createSitemap = (
+  features,
+  blogs,
+  pages
+) => `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     ${pages
       .map((page) => {
@@ -10,26 +19,66 @@ const createSitemap = (posts) => `<?xml version="1.0" encoding="UTF-8"?>
           .replace("pages", "")
           .replace(".js", "")
           .replace(".mdx", "");
-        const route = path === "/index" ? "" : path;
+        let route;
+        console.log(path);
+        if (path === "/index") {
+          route = "";
+        } else if (path === "/business-setup-pro-services/index") {
+          route = "/business-setup-pro-services";
+        } else if (path === "/dubai-business-setup-blogs/index") {
+          route = "/dubai-business-setup-blogs";
+        } else {
+          route = path;
+        }
 
         return `
                 <url>
-                    <loc>${`https://yoursitehere.com${route}`}</loc>
+                    <loc>${`${MAIN_URL}${route}`}</loc>
                 </url>
             `;
       })
       .join("")}
+        ${features
+          .map((slug) => {
+            return `
+                    <url>
+                        <loc>${`${FEATURE_URL}/${slug}`}</loc>
+                    </url>
+                `;
+          })
+          .join("")}
+          ${blogs
+            .map((slug) => {
+              return `
+                      <url>
+                          <loc>${`${BLOG_URL}/${slug}`}</loc>
+                      </url>
+                  `;
+            })
+            .join("")}
+          
     </urlset>
     `;
 
 class Sitemap extends React.Component {
   static async getInitialProps({ res }) {
-    const request = await fetch(EXTERNAL_DATA_URL);
-    const posts = await request.json();
+    const features = FeaturePostData.map((data) => {
+      return data.slug;
+    });
+    const blogs = articles.map((data) => {
+      data.slug;
+    });
 
-    console.log(pages);
+    // const request = await fetch(EXTERNAL_DATA_URL);
+    // const posts = await request.json();
+    const pages = await globby([
+      "pages/**/*{.js,.mdx}",
+      "!pages/_*.js",
+      "!pages/api",
+      "!pages/**/[slug]*{.js,.mdx}",
+    ]);
     res.setHeader("Content-Type", "text/xml");
-    res.write(createSitemap(posts));
+    res.write(createSitemap(features, blogs, pages));
     res.end();
   }
 }
